32f735ddafe4582be1912e6118df83b5d4349f91
srisailamnambu <srisailam.nambu515@gmail.com>
2025-07-19 22:10:25 +0530

    Add files via upload

---
+++ NCStudent.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self,name):
+        self.__name = name
+    def display(self):
+        print(f'student name in class = {self.__name}')
+
+s1=Student('Rama')
+s1.display()
+s1.__name = 'Krishna'
+print(f'stundet name otuside class - {s1.__name}')
+s1.display()
